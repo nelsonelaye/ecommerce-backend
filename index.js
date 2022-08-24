2378376cfa4492999f51969897a638daa5abfa44
nelsonelaye <nelsonelaye@gmail.com>
2022-08-24 22:14:17 +0100

    export app

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const port = 2334;
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
-	res.send("Welcome to my ecommerce app");
+  res.send("Welcome to my ecommerce app");
 });
 app.use("/api/product", require("./Router/productRoute"));
 app.use("/api", require("./Router/userRouter"));
 app.listen(port, () => {
-	console.log(`app is listening to ${port}`);
+  console.log(`app is listening to ${port}`);
 });
+
+module.exports = app;
