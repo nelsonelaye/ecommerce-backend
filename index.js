f9625c64c512d810c7802a204da5a2eb8086c73a
nelsonelaye <nelsonelaye@gmail.com>
2025-06-19 13:08:05 +0100

    refactor: add more comprehensive cors config

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const cors = require("cors");
 const port = 2334;
 
 const app = express();
-app.use(express.json());
 app.use(cors({
-    origin:"*"
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+  credentials: false
 }));
+
+app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to my ecommerce app");
