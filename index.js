909babf7328f6346e4cd9eefc03a37dc42fdcdcc
nelsonelaye <nelsonelaye@gmail.com>
2025-06-21 22:12:39 +0100

    chore: add frontend url origin

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const cors = require("cors");
 const port = 2334;
 
 const app = express();
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
-  credentials: false
-}));
+app.use(
+  cors({
+    origin: "https://soun-ecommerce.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+    credentials: false,
+  })
+);
 
 app.use(express.json());
-
 
 app.get("/", (req, res) => {
   res.send("Welcome to my ecommerce app");
